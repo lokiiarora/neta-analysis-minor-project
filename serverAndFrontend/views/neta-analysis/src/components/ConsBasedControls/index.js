1d2a9938f08c3545c3847c8163c7e98866c0d5f5
lokeshrajarora <lokesh.slg06@gmail.com>
2017-09-08 15:34:13 +0530

    Some more changes to the existing ConsBasedControls, all other stuff will act accordingly

--- serverAndFrontend/views/neta-analysis/src/components/ConsBasedControls/index.js
+++ serverAndFrontend/views/neta-analysis/src/components/ConsBasedControls/index.js
@@ -1,28 +1,54 @@
 import React , {Component} from 'react';
-import {Icon, Step, Segment, Dimmer, Loader} from 'semantic-ui-react';
+import { Step, Segment, Dimmer, Loader} from 'semantic-ui-react';
 import _ from 'underscore';
 import axios from 'axios';
+import SpecialSelect from '../SpecialSelect/';
 import {BASE_URI} from '../../env';
 
 export default class ConsBasedControls extends Component {
     state = {
         active:"Action",
-        payload:{step1:{},step2:{}, step3:{}},
+        payload:[],
         loading:false,
         totalSelect:false,
-        stepActive:[true,false,false]
+        stepSelectedOptions:[null,null,null],
+        summary:[
+            {completed:false,active:true, title: "What do you want to find?", description: "Select action.."},
+            {completed:false, active:false ,title: "Select State", description: "Select coressponding state"},
+            {completed:false, active:false ,title: "Select constituency", description: "Select constituency.."}
+        ]
     };
 
     componentDidMount = () => {
         this.setState({loading:true});
         this._loadInitialData();
     }
+    _showActive = (summary) => {
+            let renderIndex;
+            _.chain(summary)
+            .map((oneStep,index) => {
+                if(oneStep.active){
+                    renderIndex = index;
+                    return index
+                }
+            });
+            console.log(renderIndex);
+            return renderIndex
+    }
+    
+    _currentPayload = () => {
+        let index = this._showActive();
+        return this.state.payload[index];
+    }
+    
+    _onChildrenSelect = (e) => {
+        console.log(e);
+    }
 
     _loadInitialData = () => {
         axios.get(BASE_URI + "list/activities").then((res) => {
-            // console.log(res);
-            let pay = this.state.payload;
-            pay.step1 = res.data;
+            let pay = [];
+            pay.push(res.data.activities);
             this.setState({payload:pay, loading:false})
         }).catch((err) => {
             console.log(err);
@@ -30,6 +56,7 @@ export default class ConsBasedControls extends Component {
     }
     
     render(){
+        let summary = this.state.summary;
         return (
             <Segment>
                 <Dimmer
@@ -38,9 +65,22 @@ export default class ConsBasedControls extends Component {
                     <Loader>Loading</Loader>
                 </Dimmer>
 
-                <div>
-                    Hey I'm inside
-                </div>
+                <Step.Group ordered>
+                    {
+                        _.map(summary, (oneStep,index) => {
+                            let key = index+1;
+                            return (
+                                <Step completed={oneStep.completed} active={oneStep.active} key={key}>
+                                    <Step.Content>
+                                        <Step.Title>{oneStep.title}</Step.Title>
+                                        <Step.Description>{oneStep.description}</Step.Description>
+                                    </Step.Content>
+                                </Step>
+                            );
+                        })
+                    }
+                </Step.Group>
+                <SpecialSelect index={this._showActive()} payload={this._currentPayload()} onSelect={this._onChildrenSelect} />
             </Segment> 
         );
     }
---
+++ serverAndFrontend/views/neta-analysis/src/components/SpecialSelect/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import _ from 'underscore';
+
+class SpecialSelect extends Component {
+    render() {
+        const payload = this.props.payload;
+        console.log(payload)
+        return (
+            <div/>
+        );
+    }
+}
+
+export default SpecialSelect;
